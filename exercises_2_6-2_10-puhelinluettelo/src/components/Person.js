796068ee7d9e07af7920bee116efd7d5ce34c3f0
Perttu Punakallio <perttu.punakallio@gmail.com>
2018-10-04 22:46:38 +0300

    Finish exercise 2.11

--- exercises_2_6-2_10-puhelinluettelo/src/components/Person.js
+++ exercises_2_6-2_10-puhelinluettelo/src/components/Person.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Person = ({ person }) => {
   return (
+  <table>
+  <tbody>
     <tr>
     	<th>{person.name}</th>
     	<th>{person.number}</th>
     </tr>
+  </tbody>
+  </table>
   )
 }
 
